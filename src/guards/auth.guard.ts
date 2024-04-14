4a176f697bd40b6523efbc980dccf49d4c3d2c2b
Lucas-Winicius <lucasclucas7@gmail.com>
2024-04-14 17:54:40 -0300

    params to header

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -6,7 +6,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const code = context.switchToHttp().getRequest().query.authCode;
+    const code = context.switchToHttp().getRequest().headers.token;
     return code === process.env.AUTH_CODE;
   }
 }
